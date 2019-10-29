b199c773d24261b01f8fab4fe2f590bc887479ab
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-10-29 14:43:10 -0700

    completed mvp and stretch goal

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// 1: mouseover
+// 1, 2: mouseover, mouseleave and click prevent default
 const navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(link => {
     link.addEventListener('mouseover', () => {
@@ -9,9 +9,12 @@ navLinks.forEach(link => {
     link.addEventListener('mouseleave', () => {
         TweenMax.to(link, 0.3, {scale: 1.0});
     });
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
 });
 
-// 2: keydown 
+// 3: keydown 
 //(x: make image rotate on y and swap image halfway)
 let random = true;
 const letsGoImg = document.querySelector('.content-section img');
@@ -28,14 +31,11 @@ document.addEventListener('keydown', () => {
     }
 });
 
-// 3: wheel
+// 4: wheel
 const images = document.querySelectorAll('img');
 images.forEach(img => {
     img.addEventListener('wheel', (event) => {
         event.preventDefault();
-
-        // scale += event.deltaY * -0.01;
-        // img.style.transform = `scale(${scale})`;
         TweenMax.to(img, 0.1, {rotation:event.deltaY*0.1, borderRadius:Math.abs(event.deltaY)*1});
     })
     img.addEventListener('mouseleave', () => {
@@ -43,7 +43,7 @@ images.forEach(img => {
     })
 })
 
-// 4, 5, 6: drag & drop
+// 5, 6, 7: drag & drop
 const dragBoxes = document.querySelectorAll('.imgD');
 let dragged;
 dragBoxes.forEach(box => {
@@ -62,14 +62,56 @@ dragBoxes.forEach(box => {
 
     document.addEventListener('drop', (event) => {
         event.preventDefault();
-        console.log("test");
         if (event.target.className == "imgBox") {
             event.target.style.background = "";
             dragged.parentNode.removeChild( dragged );
             event.target.appendChild( dragged );
           }
     }, false);
-    
 })
 
-// 7: scroll
+// 8: scroll
+
+const texts = document.querySelectorAll('p, h1, h2, h3, h4');
+
+window.addEventListener('scroll', () => {
+    console.log("scroll");
+    let randomColor = getRandomColor();
+        texts.forEach(p =>{
+        TweenMax.to(p, 0.5, {color:randomColor});
+    });
+});
+
+const getRandomColor = function(){
+    var letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// 9: load
+window.addEventListener('load', () => {
+    images.forEach(img =>{
+        TweenMax.from(img, 1, {x:1000});
+    })
+    texts.forEach(text =>{
+        TweenMax.from(text, 1, {x:-1000});
+    })
+})
+
+// 10: dblclick and nesting
+const navBar = document.querySelector('.main-navigation');
+const logoName = document.querySelector('.logo-heading');
+
+logoName.style.padding = "10px";
+
+navBar.addEventListener('dblclick', (e) => {
+    navBar.style.background = getRandomColor();
+})
+
+logoName.addEventListener('dblclick', (e) => {
+    logoName.style.background = getRandomColor();
+    e.stopPropagation();
+})
